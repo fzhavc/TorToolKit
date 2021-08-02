e1627f353932af23f63bc84fe0ecafdb4e20fa43
Mⷨoͦns͛ᴛⷮeͤrͬ Zeͤrͬoͦ <69518719+Monster-ZeroX@users.noreply.github.com>
2021-08-02 14:44:59 +0530

    Update ttk_ytdl.py

--- tortoolkit/core/ttk_ytdl.py
+++ tortoolkit/core/ttk_ytdl.py
@@ -1,5 +1,4 @@
 
-from ..functions.tele_upload import upload_handel
 from ..core.getVars import get_val
 from ..functions.rclone_upload import get_config,rclone_driver
 from functools import partial
